442579dc1c93fe46550cf19b7adb4076cf15dd9d
DavidPavel1 <71712339+DavidPavel1@users.noreply.github.com>
2020-11-05 09:31:30 -0500

    changed

--- PiratesStep2.java
+++ PiratesStep2.java
@@ -25,25 +25,34 @@ public class PiratesStep2 {
 	 * Declare the objects and variables that you want to access across
 	 * multiple methods.
 	 */
-
+	
+	static JLabel pirateShip;
+	
+	static int score = 0;
+	static JLabel scoreLabel;
 
 	/**
 	 * CREATE MAIN WINDOW
 	 * This method is called by the main method to set up the main GUI window.
 	 */
 	private static void createMainWindow () {
+		
+		
 		// Create and set up the window.
 		JFrame frame = new JFrame ("Pirates");
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.setResizable (false);
 
+		
 		// The panel that will hold the components in the frame.
 		JPanel contentPane = new JPanel ();
 		contentPane.setPreferredSize(new Dimension(950, 400));
 
+		
 		// Making the content pane use BorderLayout
 		contentPane.setLayout(new BorderLayout());
 
+		
 		// Make the side panel
 		JPanel sideBar = new JPanel();
 		sideBar.setLayout(new BoxLayout(sideBar, BoxLayout.PAGE_AXIS));
@@ -51,20 +60,24 @@ public class PiratesStep2 {
 		sideBar.setBorder(new EmptyBorder(20, 20, 20, 20));
 		contentPane.add(sideBar, BorderLayout.EAST);
 
+		
 		// Make the score title
 		JLabel scoreTitle = new JLabel("Score");
 		scoreTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
 		sideBar.add(scoreTitle);
 		sideBar.add(Box.createRigidArea(new Dimension(135, 10)));
 
+		
 		// Make the score label
-		JLabel scoreLabel = new JLabel("0");
+		scoreLabel = new JLabel("0");
 		scoreLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		sideBar.add(scoreLabel);
 
+		
 		// Add the filler "glue"
 		sideBar.add(Box.createVerticalGlue());
 
+		
 		// Make sidebar title
 		JLabel actionsLabel = new JLabel("Actions");
 		actionsLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -73,6 +86,7 @@ public class PiratesStep2 {
 		sideBar.add(actionsLabel);
 		sideBar.add(Box.createRigidArea(new Dimension(135, 10)));
 
+		
 		// Make sidebar buttons
 		JButton newGameButton = new JButton("New Game");
 		newGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -89,28 +103,34 @@ public class PiratesStep2 {
 		quitButton.addActionListener(new QuitButtonListener());
 		sideBar.add(quitButton);
 
+		
 		// Make the center "map" panel
 		JLayeredPane mapPanel = new JLayeredPane();
 		contentPane.add(mapPanel, BorderLayout.CENTER);
 
+		
 		// Get the map image
 		JLabel mapImage = new JLabel(new ImageIcon("resources/world-map-animals.jpg"));
 		mapPanel.add(mapImage, Integer.valueOf(-300));
 		mapImage.setSize(775, 400);
 		mapImage.setLocation(0, 0);
 
+		
 		// Create the pirate ship
-		JLabel pirateShip = createScaledImage("resources/pirate-ship.png", 40, 40);
+		pirateShip = createScaledImage("resources/pirate-ship.png", 40, 40);
 		pirateShip.setSize(40, 40);
 		Random randomGenerator = new Random();
 		int pirateX = randomGenerator.nextInt(735);
 		int pirateY = randomGenerator.nextInt(360);
 		pirateShip.setLocation(pirateX, pirateY);
+		pirateShip.addMouseListener(new ShipMouseHandler());
 		mapPanel.add(pirateShip);
 
+		
 		// Add the panel to the frame
 		frame.setContentPane(contentPane);
 
+		
 		//size the window.
 		frame.pack();
 		frame.setLocationRelativeTo(null);
@@ -148,5 +168,37 @@ public class PiratesStep2 {
 		}
 	}
 	
+	
+	/** This listens for mouse interactions with the pirate ship*/
+    private static class ShipMouseHandler implements MouseListener {
+
+        public void mouseClicked (MouseEvent event) {
+        	// Score up by 1 
+        	score ++;
+        	scoreLabel.setText(String.valueOf(score));
+        	
+        	// Ship to disappear
+        	pirateShip.setVisible(false);
+        }
+
+        public void mousePressed (MouseEvent event) {
+
+            // TODO: Actions to take when the component is clicked.
+
+        }
+
+        public void mouseReleased (MouseEvent event) {
+        }
+
+        public void mouseEntered (MouseEvent event) {
+        }
+        
+        public void mouseExited (MouseEvent event) {
+
+   
+
+        }
+
+    }
 	
 }
